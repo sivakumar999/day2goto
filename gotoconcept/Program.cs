5c9f72a72aab4c9557887280b8a4c1484b906712
sivakumar999 <sivakumar59498@gmail.com>
2023-07-11 11:58:16 +0530

    first

---
+++ gotoconcept/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gotoconcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName;
+            Again:
+            Console.WriteLine("Entere user name");
+            userName = Console.ReadLine();
+            if(userName.Length>=6)
+            {
+                Console.WriteLine("Welcome : \t " + userName);
+            }
+            else
+            {
+                Console.WriteLine("Inavalid User Name");
+                goto Again;
+            }
+            Console.ReadKey();
+        }
+    }
+}
